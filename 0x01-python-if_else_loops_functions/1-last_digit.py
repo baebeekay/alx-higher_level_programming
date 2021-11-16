7a3926fbfc0060283399c322061c50ced3046c4f
baebeekay <baebeekay6.com>
2021-11-16 09:13:42 -0800

    Prints the last digit

---
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+import random
+number = random.randint(-10000, 10000)
+if number < 0:
+    last_num = number % -10
+else:
+    last_num = number % 10
+print('Last digit of {:d} is {:d}'.format(number, last_num), end=" ")
+if last_num > 5:
+    print('and is greater than 5')
+elif last_num == 0:
+    print('and is 0')
+else:
+    print('and is less than 6 and not 0')
