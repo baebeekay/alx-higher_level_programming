a9e76c416e1328f7e5948774c47685abcfd5c9fe
baebeekay <baebeekay6.com>
2021-11-16 12:12:46 -0800

    Prints all characters in uuppercase

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,3 +1,9 @@
 #!/usr/bin/python3
 def uppercase(str):
-    print("{:s}".format(str.upper()))
+    str1 = ''
+    for i in str:
+        if(i >= 'a' and i <= 'z'):
+            str1 = str1 + chr((ord(i) - 32))
+        else:
+            str1 = str1 + i
+    print("{:s}".format(str1))
