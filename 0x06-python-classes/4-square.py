1b1c7e32b8529424f41748b6396176a7cd6d7b25
baebeekay <baebeekay6.com>
2021-12-13 10:57:33 -0800

    Getter and Setter

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"4-square.py - defines the area of a square"
+
+
+class Square:
+    """
+    Class that defines the area a square
+    If the size is not an integer raises a Type Error
+    If the size is less than zero raises a Value error
+    Args:
+        Size (int): Private, Size of the square
+    """
+    def __init__(self, size=0):
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+
+    def area(self):
+        area = self.__size * self.__size
+        return area
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
