21e1179bd269627641b0283087b397d50c5c5000
baebeekay <baebeekay6.com>
2021-12-15 17:28:11 -0800

    Error fix

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -8,8 +8,8 @@ class Rectangle:
     by width and height
     """
     def __init__(self, width=0, height=0):
-        self.__width = width
-        self.__height = height
+        self.width = width
+        self.height = height
 
     @property
     def width(self):
