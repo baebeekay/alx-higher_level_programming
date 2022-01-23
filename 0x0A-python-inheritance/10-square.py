1a435661c973bf7c30d407c57871651388d20853
baebeekay <baebeekay6.com>
2022-01-23 15:20:11 -0800

    Base Geometry inherit

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -13,11 +13,9 @@ class Square(Rectangle):
         self.integer_validator("size", size)
         self.__size = size
 
-
     def area(self):
         area = self.__size * self.__size
         return area
-
 
     def __str__(self):
         return ("[Rectangle] {}/{}".format(self.__size, self.__size))
