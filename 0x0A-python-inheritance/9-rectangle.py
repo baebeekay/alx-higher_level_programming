ba9b6a9da23acc22cc88cf65980b160ef890d0f5
baebeekay <baebeekay6.com>
2022-01-23 15:17:00 -0800

    Base Geometry inherit

---
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""  9-rectangle.py - module"""
+
+
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """
+    a class Rectangle that inherits from BaseGeometry
+    """
+
+    def __init__(self, width, height):
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
+
+    def area(self):
+        area = self.__width * self.__height
+        return area
+
+    def __str__(self):
+        return("[Rectangle] {}/{}".format(self.__width, self.__height))
