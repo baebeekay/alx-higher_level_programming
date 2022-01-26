cc48e0821ecdc5843ac2313aab155fd7bfcd4af6
baebeekay <baebeekay6.com>
2022-01-26 09:13:19 -0800

    Append to files

---
+++ 0x0B-python-input_output/2-append_write.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+""" 2-append_write.py - module """
+
+
+def append_write(filename="", text=""):
+    """
+    a function that appends a string at the end of a text
+    file (UTF8) and returns the number of characters added:
+    """
+    with open(filename, 'a', encoding='UTF8') as f:
+        return f.write(text)
