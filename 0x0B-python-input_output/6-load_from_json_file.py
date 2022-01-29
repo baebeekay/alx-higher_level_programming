56edb1880bb697bbd8cff93c55d7ff57faed9334
baebeekay <baebeekay6.com>
2022-01-28 18:18:19 -0800

    Load from json file

---
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+""" 6-load_from_json_file.py - module """
+
+
+import json
+""" json module """
+
+
+def load_from_json_file(filename):
+    """
+    a function that creates an Object from a “JSON file”
+    """
+    with open(filename) as f:
+        json.load(f)
