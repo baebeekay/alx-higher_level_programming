41c8134d30d28f5854db19acee647bfc69f19d3f
baebeekay <baebeekay6@gmail.com>
2022-02-24 09:57:32 -0800

    Rectangle

---
+++ 0x08-python-more_classes/6-rectangle.py
@@ -0,0 +1,71 @@
+#!/usr/bin/python3
+""" 6-rectangle.py - defines a rectangle   """
+
+
+class Rectangle:
+    """
+    Class that defines the a rectangle
+    by width and height
+    return area and perimeter
+    prints the rectangle
+    """
+    number_of_instances = 0
+
+    def __init__(self, width=0, height=0):
+        self.__width = width
+        self.__height = height
+        Rectangle.number_of_instances += 1
+    
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        elif value < 0:
+            raise ValueError("width must be >= 0")
+        else:
+            self.__width = value
+
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if type(value) is not int:
+            raise TypeError("height must be an integer")
+        elif value < 0:
+            raise ValueError("height must be >= 0")
+        else:
+            self.__height = value
+
+    def area(self):
+        area = self.__width * self.__height
+        return area
+
+    def perimeter(self):
+        perimeter = 0
+        if self.__width == 0 or self.__height == 0:
+            return perimeter
+        else:
+            perimeter = (self.__width + self.__height) * 2
+            return perimeter
+
+    def __str__(self):
+        sign = ""
+        if self.__width == 0 or self.__height == 0:
+            return sign
+        for i in range(self.__height - 1):
+            sign += ("#" * self.__width) + "\n"
+        sign += ("#" * self.__width)
+        return sign
+
+    def __repr__(self):
+        return "Rectangle({}, {})".format(self.__width, self.__height)
+
+    def __del__(self):
+        Rectangle.number_of_instances -= 1
+        print("Bye rectangle...")
